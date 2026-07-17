7448ca55a3fed84dc2dcbee174ee63dadbab6f51
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-07-17 10:50:45 +0530

    new button

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ const App = () => {
         className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 active:scale-95 transition duration-200">Click Me!</button>
       <button onClick={handleButtonClick}
         className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 active:scale-95 transition duration-200">Buy Now!</button>
+      <button onClick={handleButtonClick}
+        className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 active:scale-95 transition duration-200">add to cart</button>
+
     </div>
   )
 }
