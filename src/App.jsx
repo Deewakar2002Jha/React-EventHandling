7441385fcc570abdc6c8f7983100d865aface288
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-07-17 10:44:11 +0530

    Event Handling using reacts

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  function handleButtonClick() {
+    // console.log('Button clicked!');
+    // alert('Button clicked!');
+    // console.log(event);
+    // console.log(event.type);
+    console.log(event.target)
+
+
+  }
+  return (
+    <div>
+      <button onClick={handleButtonClick}
+        className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 active:scale-95 transition duration-200">Click Me!</button>
+    </div>
+  )
+}
+
+export default App
